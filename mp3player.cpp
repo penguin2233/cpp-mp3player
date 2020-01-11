91e28dfeb4f41e3a98f09710c93d4b5c863a5921
penguin2233 <tuxace45@gmail.com>
2020-01-11 13:26:41 +1100

    playback time

--- mp3player.cpp
+++ mp3player.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <Windows.h>
+#include <thread>
+#include <chrono>
 #include <bass.h>
 #include <bassflac.h>
+
+void clearscreen()
+{
+	HANDLE                     hStdOut;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD                      count;
+	DWORD                      cellCount;
+	COORD                      homeCoords = { 0, 0 };
+
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+	/* Get the number of cells in the current buffer */
+	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+	cellCount = csbi.dwSize.X * csbi.dwSize.Y;
+
+	/* Fill the entire buffer with spaces */
+	if (!FillConsoleOutputCharacter(
+		hStdOut,
+		(TCHAR)' ',
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Fill the entire buffer with the current colors and attributes */
+	if (!FillConsoleOutputAttribute(
+		hStdOut,
+		csbi.wAttributes,
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Move the cursor home */
+	SetConsoleCursorPosition(hStdOut, homeCoords);
+}
 
 void stats()
 {
@@ -30,23 +69,45 @@ void cpuusage()
 void playflac()
 {
 	std::cout << "loading file \n";
-	HSTREAM stream = BASS_FLAC_StreamCreateFile(FALSE, "test.FLAC", 0, 0, 0);
+	HSTREAM stream = BASS_FLAC_StreamCreateFile(FALSE, "test.FLAC", 0, 0, BASS_STREAM_PRESCAN);
 	BASS_ChannelPlay(stream, true);
-	for (;;)
+	clearscreen();
+	std::cout << "playing file \n";
+
+	while (BASS_ChannelIsActive(stream) == BASS_ACTIVE_PLAYING)
 	{
-		cpuusage();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		clearscreen();
+		QWORD totaltimebyte = BASS_ChannelGetLength(stream, BASS_POS_BYTE);
+		int totaltime = BASS_ChannelBytes2Seconds(stream, totaltimebyte);
+		QWORD currenttimebyte = BASS_ChannelGetPosition(stream, BASS_POS_BYTE);
+		int currenttime = BASS_ChannelBytes2Seconds(stream, currenttimebyte);
+		std::cout << currenttime << " / " << totaltime << '\n';
 	}
+	std::cout << "playback has finished use enter to exit";
+	getchar();
 }
 
 void playmp3()
 {
 	std::cout << "loading file \n";
-	HSTREAM stream = BASS_StreamCreateFile(FALSE, "test.mp3", 0, 0, 0);
+	HSTREAM stream = BASS_StreamCreateFile(FALSE, "test.mp3", 0, 0, BASS_STREAM_PRESCAN);
 	BASS_ChannelPlay(stream, true);
-	for (;;)
+	clearscreen();
+	std::cout << "playing file \n";
+
+	while( BASS_ChannelIsActive(stream) == BASS_ACTIVE_PLAYING )
 	{
-		cpuusage();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		clearscreen();
+		QWORD totaltimebyte = BASS_ChannelGetLength(stream, BASS_POS_BYTE);
+		int totaltime = BASS_ChannelBytes2Seconds(stream, totaltimebyte);
+		QWORD currenttimebyte = BASS_ChannelGetPosition(stream, BASS_POS_BYTE);
+		int currenttime = BASS_ChannelBytes2Seconds(stream, currenttimebyte);
+		std::cout << '\r' << currenttime << " / " << totaltime << '\n';
 	}
+	std::cout << "playback has finished use enter to exit";
+	getchar();
 }
 
 int main()
